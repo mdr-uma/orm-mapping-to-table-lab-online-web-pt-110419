8fadc7017227b23572a89df8d716f4fa66577bcb
Uma Manandhar <um_2003_8@hotmail.com>
2020-01-31 23:54:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -40,8 +40,8 @@ class Student
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
 
-  def self.create(name)
-    student = student.new(name)
+  def self.create(name:, grade:)
+    student = student.new(name, grade)
     student.save
     student
   end
