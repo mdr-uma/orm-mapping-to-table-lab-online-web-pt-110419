3a64f779ef9463415747f4b2283eef01df05087d
Uma Manandhar <um_2003_8@hotmail.com>
2020-01-31 23:42:25 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -24,11 +24,7 @@ class Student
 
   def self.drop_table
     sql = <<-SQL
-      DROP TABLE IF EXISTS students(
-        id INTEGER PRIMARY KEY,
-        name TEXT,
-        grade TEXT
-      )
+      DROP TABLE students
       SQL
        DB[:conn].execute(sql)
   end
